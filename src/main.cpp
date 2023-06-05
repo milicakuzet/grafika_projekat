a4d439b6c697eff91135dff9450b73be08ecffcb
Milica <mi20064@alas.matf.bg.ac.rs>
2023-06-06 00:48:08 +0200

    finalna izmena

--- src/main.cpp
+++ src/main.cpp
@@ -118,7 +118,7 @@ int main() {
 
     // glfw window creation
     // --------------------
-    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Nebo", NULL, NULL);
     if (window == NULL) {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
